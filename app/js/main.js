2b90fb68c777fadd83062169847ddc165ffb7ab2
Sedoplatov <sedoplatov@gmail.com>
2021-04-01 14:07:39 +0600

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 $(function(){
 
+  $('.product-one__item-num').styler();
+
   $(".filter-products__star").rateYo({
     starWidth: "11px",
     readOnly: true,
@@ -7,13 +9,13 @@ $(function(){
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00"
   });
-
-  $(".shop-product__list-item--star").rateYo({
+  
+  $(".shop-product__list-item--star, .product-one__star").rateYo({
     starWidth: "18px",
     readOnly: true,
     spacing: "6px",
     normalFill: "#d6d6d6",
-    ratedFill: "#ffcc00"
+    ratedFill: "#ffcc00",
   });
 
   $(".filter-price__input").ionRangeSlider({
@@ -28,6 +30,20 @@ $(function(){
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
+  });
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    vertical: true,
+    draggable: false,
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
   });
 
   $('.top-slider__inner').slick({
